1dc62dbfc9627a2eb2fa9b044d49ea6139be90ed
Gabor Angyal <gabor.angyal@codesharp.dev>
2025-03-10 16:12:30 +0100

    values

--- third-party-brushes/shBrushNginx.js
+++ third-party-brushes/shBrushNginx.js
@@ -1,33 +1,35 @@
 SyntaxHighlighter.brushes.Nginx = function()
 {
-	var mainDirectives = 'http server events location include if map';
+	var mainDirectives = /(^|\s)(http|server|events|location|include|if|map)\s/g;
 	
-	var directives = 'listen server_name root index try_files deny expires set return add_header client_max_body_size';
+	var directives = /\s(listen|server_name|root|index|try_files|deny|expires|set|return|add_header|client_max_body_size|default)\s/g;
 
 	this.regexList = [
 		// Comments
-		{ regex: new RegExp('#.*$', 'gm'),                          css: 'comments' },
+		{ regex: new RegExp('#.*$', 'gm'),                           css: 'comments' },
 		
 		// Strings
-		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,     css: 'string' },
-		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,     css: 'string' },
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,      css: 'string' },
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,      css: 'string' },
 		
 		// Block directives (like http, server, location)
-		{ regex: new RegExp(this.getKeywords(mainDirectives), 'gm'), css: 'keyword' },
+		{ regex: mainDirectives,                                     css: 'keyword' },
 		
 		// Regular directives
-		{ regex: new RegExp(this.getKeywords(directives), 'gm'),    css: 'functions' },
+		{ regex: directives,                                         css: 'functions' },
 		
 		// Variables
 		{ regex: /\$[a-z0-9_]+/g,                                    css: 'variable' },
+		{ regex: /[0-9]+[GMKksm]?/g,                                 css: 'value' },
+		{ regex: /\s(POST|GET)\s/g,                                  css: 'value' },
 
-		{ regex: /fastcgi_[a-z_]+/g,                                css: 'functions'},
+		{ regex: /fastcgi_[a-z_]+/g,                                 css: 'functions'},
 		
-		{ regex: /[\{\}]/g,                                         css: 'color1' },
-		{ regex: /;/g,                                              css: 'color1' },
+		{ regex: /[\{\}]/g,                                          css: 'color1' },
+		{ regex: /;/g,                                               css: 'color1' },
 		
-		{ regex: /~\*?\s(\([^{"()]+\)|[^{"()]+)+/g,                 css: 'regex' },
-		{ regex: /~\*?\s"(\([^{"()]+\)|[^{"()]+)+"/g,               css: 'regex' }
+		{ regex: /~\*?\s(\([^{"()]+\)|[^{"()]+)+/g,                  css: 'regex' },
+		{ regex: /~\*?\s"(\([^{"()]+\)|[^{"()]+)+"/g,                css: 'regex' }
 	];
 };
 
